c0fc595543fe9c24e7f17d0507c9e5a9a11bd15c
GadiegoN <ngadiego@gmail.com>
2021-09-25 12:21:16 -0300

    :package: craindo hook de listagem e paginacao

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,39 +2,16 @@ import {Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, T
 
 import Link from 'next/link';
 
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useEffect } from "react";
+import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header/Index";
-import { useQuery } from 'react-query';
 
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
 
-    const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-        const { data } = await api.get('users')
-        
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric',
-                })
-            }    
-        });
-
-        return users;
-    }, {
-        staleTime: 1000 * 5 // 5 secunds
-    })
-
-    // console.log(query);
+    const { data, isLoading, isFetching, error } = useUsers();
 
     const isWideVersion = useBreakpointValue({
         base: false,
