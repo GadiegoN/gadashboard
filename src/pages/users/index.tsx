209ba6e17a6250ef4eed7204fd937c2849fbc297
GadiegoN <ngadiego@gmail.com>
2021-09-26 13:50:03 -0300

    :package: Finalizado paginacao e criando users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,6 @@
-import {Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import {Box, Button, Checkbox, Flex, Heading, Icon, Link, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 
-import Link from 'next/link';
+import NextLink from 'next/link';
 
 import { RiAddLine } from "react-icons/ri";
 import { useState } from "react";
@@ -8,7 +8,10 @@ import { Header } from "../../components/Header/Index";
 
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { useUsers } from "../../services/hooks/useUsers";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
+import { GetServerSideProps } from "next";
 
 export default function UserList() {
     const [page, setPage] = useState(1);
@@ -18,6 +21,16 @@ export default function UserList() {
         base: false,
         lg: true,
     });
+
+    async function handlePrefetchUser(userId: string) {
+        await queryClient.prefetchQuery(['user', userId], async () => {
+            const response = await api.get(`users/${userId}`)
+
+            return response.data;
+        }, {
+            staleTime: 1000 * 60 * 10, // 10 minutes
+        })
+    }
 
     return (
         <Box>
@@ -32,17 +45,17 @@ export default function UserList() {
                             Usuários
                             { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
                         </Heading>
-                        <Link href="/users/create" passHref>
-                        <Button
-                            as="a"
-                            size="sm"
-                            fontSize="sm"
-                            colorScheme="pink"
-                            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-                        >
-                            Criar novo
-                        </Button>
-                        </Link>
+                        <NextLink href="/users/create" passHref>
+                            <Button
+                                as="a"
+                                size="sm"
+                                fontSize="sm"
+                                colorScheme="pink"
+                                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                            >
+                                Criar novo
+                            </Button>
+                        </NextLink>
                     </Flex>
 
                     { isLoading ? (
@@ -75,7 +88,12 @@ export default function UserList() {
                                                 </Td>
                                                 <Td>
                                                     <Box>
-                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Link 
+                                                            color="purple.400"
+                                                            onMouseEnter={() => handlePrefetchUser(user.id)}
+                                                        >
+                                                            <Text fontWeight="bold">{user.name}</Text>
+                                                        </Link>
                                                         <Text fontSize="sm" color="gray.300">{user.email}</Text>
                                                     </Box>
                                                 </Td>
@@ -109,3 +127,13 @@ export default function UserList() {
         </Box>
     );
 }
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//     const { users, totalCount } = await getUsers(1)
+
+//     return {
+//         props: {
+//             users,
+//         }
+//     }
+// }
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions } from "react-query";
 import { api } from "../api";
 
 type User = {
@@ -40,7 +40,7 @@ type GetUsersResponse = {
 
 export function useUsers(page: number) {
     return useQuery(['users', page], () => getUsers(page), {
-        staleTime: 1000 * 5 // 5 secunds
+        staleTime: 1000 * 60 * 10, // 10 minutes
     })
     // console.log(query);
 }
